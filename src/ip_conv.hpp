ca0b14757d41da6826752c8616f9b6cd21279039
shniu <xiaoniu198903@126.com>
2018-08-17 19:10:47 +0800

    ip conv

---
+++ src/ip_conv.hpp
@@ -0,0 +1,9 @@
+
+#ifndef FLUENTCPP_IP_CONV
+#define FLUENTCPP_IP_CONV
+
+// ip to hex string
+char* ip_to_hex(const char* &ip);
+
+#endif  /// end of define
+
---
+++ src/tour_of_cpp.hpp
@@ -0,0 +1,35 @@
+
+
+#include <iostream>
+
+class Vector {
+
+public:
+    Vector(int s):elem{ new double[s] },sz{s} {}
+
+	int size() {
+		return sz;
+	}
+
+	double& operator[](int i) {
+		return elem[i];
+	}
+
+private:
+
+    int sz;
+	double * elem;
+
+}
+
+
+enum class Color {
+    red, blue, green
+};
+
+enum Traffic {
+    red, blue, green
+};
+
+
+
